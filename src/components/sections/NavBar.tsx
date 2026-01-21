344a43461542cb7c23aa7bde955c1f8978b746ca
asphzeera <asaphobrito@gmail.com>
2026-01-21 16:25:20 -0300

    Refatoração da Navbar, tirei os <Links> para refatorar de forma mais simples, porém vou readicionar logo depois.

--- src/components/sections/NavBar.tsx
+++ src/components/sections/NavBar.tsx
@@ -7,33 +7,28 @@ import { useState } from "react";
 
 export default function NavBar() {
   const [alterarModoParaRoxo, setAlterarModoParaRoxo] = useState(false);
-  
-    return (
-      <header>
-        <div className={"background-white"}>
-          {/* <Image className={"Logo aqui"} src={""} alt=""/> */}
-          <div>
-            <Link href={"#Sobre nós"}>
-              <p className={"txt-rosa"}>Sobre nós</p>
-            </Link>
-            <Link href={"#Serviços"}>
-              <p className={"txt-rosa"}>Serviços</p>
-            </Link>
-            <Link href={"#Benefícios"}>
-              <p className={"txt-rosa"}>Benefícios</p>
-            </Link>
-            <Link href={"#Endereço"}>
-              <p className={"txt-rosa"}>Endereço</p>
-            </Link>
-            <Link href={"#linkWhatsapp"} className={"sem-line background-rosa-escuro txt-white"}>
-            <Image src={IconeWhatsapp} alt="Icone do Whatsapp" className={"icone"}/>
-              Fale Conosco
-            </Link>
-            <div className={"modo-escuro efeito-pointer"} onClick={() => setAlterarModoParaRoxo(!alterarModoParaRoxo)}>
-              <BiAdjust style={alterarModoParaRoxo ? {color: '#AA6BC8'} : {color: '#463E70'}} className="botaoModo"/>
-            </div>
-          </div>
+
+  return (
+    <nav className="fixed top-2 inset-x-0 z-50 flex justify-center px-2">
+      <div className=" w-full flex items-center justify-between bg-black shadow-lg rounded-2xl">
+        <Image className="flex w-14 h-14" src={IconeWhatsapp} alt="Logo" />
+        <ul className="text-white absolute left-1/2 -translate-x-1/2 flex gap-6 list-none items-center">
+          <li>Sobre nós</li>
+          <li>Serviços</li>
+          <li>Beneficios</li>
+          <li>Endereço</li>
+        </ul>
+        <div className="flex items-center gap-4 mr-5">
+          <div className="flex items-center w-60 h-9 gap-5 bg-red-700 rounded-2xl  ">
+            <Image className="flex ml-2 items-center gap-2 w-10 h-10" src={IconeWhatsapp} alt="" />
+            <p>Fale Conosco</p>
+          </div>       
+          <BiAdjust 
+            className="cursor-pointer text-2xl text-white" 
+            onClick={() => setAlterarModoParaRoxo(!alterarModoParaRoxo)}
+          />
         </div>
-      </header>
-    );
+      </div>
+    </nav>
+  );
 }
