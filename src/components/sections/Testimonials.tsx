b54b9f2442c10adfb89cdd9407332b3b203b54ae
asphzeera <asaphobrito@gmail.com>
2026-01-28 16:49:45 -0300

    Cara coisas ai

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -2,27 +2,28 @@
 import Image from "next/image";
 import imagem from "@/public/Imagem.svg"
 import { useState, useEffect } from "react";
+import amandita from "@/public/Ellipse 6.png"
+import blob from "@/public/blob(2) 1.png"
 
 export default function Testimonials(){
     function carrosselDepoimentos() {}
     return (
-        <div className="container">
-            <h2 className="titulo-txt-rosa">Depoimentos</h2>
-            <h3 className="txt-roxo-claro">veja o que nossos clientes dizem sobre nós</h3>
-
-            <div className="">
-                <div className="meia-lua-flutante"></div>
-
-                <div className="card-depoimentos">
-                    <div className="depoimento-item"></div>
-                    
-                    <div className="paleta-decoration">
-                        <div className="paleta-decoration-rotation-190deg">
-                            <Image src={imagem} alt="Maria Depill" className="imagem-mariadepill"/>
-                        </div>
+        <section className="w-full flex items-center justify-center bg-[linear-gradient(270deg,#FFFFFF_36%,#9D8BFF_86%)]">
+            <div className="flex w-full max-w-8/12 bg-blue-600">
+                <div>
+                    <div className="p-10 ">
+                        <Image src={amandita} alt="Amandita"/>
+                        <span>
+                            <h1>Amandita</h1>
+                            <h2>Esteticista</h2>
+                        </span>
                     </div>
+                    <span>
+                        “Super confortavel o ambiente,<br/>profissionais super capacitadas”
+                    </span>
                 </div>
             </div>
-        </div>
+            <Image src={blob} alt=""/>
+        </section>
     )
 }
